e537e25d8f3236e0c705515661b45367a443ac43
David Nitzsche-Bell <david@zicount.com>
2023-02-10 00:05:11 +0100

    Initial Commit

---
+++ Snacktackular/SnacktackularApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnacktackularApp.swift
+//  Snacktackular
+//
+//  Created by Zicount on 10.02.23.
+//
+
+import SwiftUI
+
+@main
+struct SnacktackularApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
